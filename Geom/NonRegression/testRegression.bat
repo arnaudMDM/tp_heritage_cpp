d102883ddf78b99dba6f1b254bb0a171a0c949c4
pfouchet <pfouchet@1f197c56-e716-4fb1-9ece-5c18971747b9>
2012-02-15 10:53:11 +0000

    git-svn-id: https://shareif.insa-lyon.fr/svn/am.tpheritagec-@87 1f197c56-e716-4fb1-9ece-5c18971747b9

--- Geom/NonRegression/testRegression.bat
+++ Geom/NonRegression/testRegression.bat
@@ -1,8 +1,8 @@
 @echo off
 set repertoire="C:\Users\Pitou\workspace\Geom\"
 ::Chemin d'accès au répertoire Geom
-set /A compteurs="0"
-
+set /A compteurEchecs="0"
+set /A compteurGlobal="0"
 if %repertoire%==[] goto end
 cd %repertoire%
 for /F %%a in ('dir /B "NonRegression/Test"') do (
@@ -11,17 +11,22 @@ for /F %%a in ('dir /B "NonRegression/Test"') do (
 
 cd NonRegression/res/
 for /F %%a in ('dir /B "../Test"') do (
-	
-	fc /C "%%a" "res%%a" | findstr "aucune" 
+	diff -I "^#.*" "%%a" "res%%a"
+	::fc /C "%%a" "res%%a" | findstr "aucune" >nul
+	set /A compteurGlobal+=1
 	if errorlevel 1 (
 		echo Le test %%a a echoue
-		set /A compteurs+=1 
+		set /A compteurEchecs+=1 
 	) else (
 		echo Le test %%a est passe) )
 
 cd ../..
 
-@echo %compteurs% test(s) echoue(s)
+if compteurEchecs LEQ 1 (
+@echo %compteurEchecs% test echoue sur %compteurGlobal%
+) else (
+@echo %compteurEchecs% tests echoues sur %compteurGlobal%
+)
 
 :end
 pause
